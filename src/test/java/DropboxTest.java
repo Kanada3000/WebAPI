3cf10b3e9d7c47800773417d69d79a15d7bccdaf
Oleksandra Kadatska <aleksandra.kadatskaya2001.asus@gmail.com>
2021-05-13 12:06:54 +0300

    first commit

---
+++ src/test/java/DropboxTest.java
@@ -0,0 +1,65 @@
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Test;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import static io.restassured.config.EncoderConfig.encoderConfig;
+
+public class DropboxTest {
+
+    private static final String TOKEN = "aLNzXu6ZQHQAAAAAAAAAAbtsqv2Nc6qEWAj1AHSbWW8qteErwl1qCqQPeyeIMP8e";
+
+
+    @Test
+    public void UploadTest() throws IOException{
+        String myJson = "{\"mode\": \"add\"," +
+                        "\"autorename\": true," +
+                        "\"mute\": false," +
+                        "\"path\": \"/message.txt\"," +
+                        "\"strict_conflict\": false}";
+
+        byte[] file = Files.readAllBytes(Paths.get("src/message.txt"));
+
+        RequestSpecification request = RestAssured.given();
+        request.config(RestAssured.config().encoderConfig(encoderConfig()
+                                            .appendDefaultContentCharsetToContentTypeIfUndefined(false)))
+                .header("Authorization", "Bearer " + TOKEN)
+                .header("Dropbox-API-Arg", myJson)
+                .header("Content-Type","application/octet-stream")
+                .body(file)
+                .when()
+                .post("https://content.dropboxapi.com/2/files/upload")
+                .then()
+                .assertThat()
+                .statusCode(200);
+    }
+
+    @Test
+    public void GetFileMetadataTest(){
+        String myJson = "{\n\t\"path\": \"/message.txt\"\n}";
+        RequestSpecification request = RestAssured.given();
+        request.header("Authorization", "Bearer " + TOKEN)
+                .header("Content-Type","application/json")
+                .body(myJson)
+                .when()
+                .post("https://api.dropboxapi.com/2/files/get_metadata")
+                .then()
+                .assertThat()
+                .statusCode(200);
+    }
+
+    @Test
+    public void DeleteTest(){
+        String myJson = "{\n\t\"path\": \"/message.txt\"\n}";
+        RequestSpecification request = RestAssured.given();
+        request.header("Authorization", "Bearer " + TOKEN)
+                .header("Content-Type","application/json")
+                .body(myJson)
+                .when()
+                .post("https://api.dropboxapi.com/2/files/delete_v2")
+                .then()
+                .assertThat()
+                .statusCode(200);
+    }
+}
